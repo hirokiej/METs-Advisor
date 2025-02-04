55a7c835c09ba7e62969666b0cfaf863141d1085
hirokiej <123009866+hirokiej@users.noreply.github.com>
2025-02-05 00:29:16 +0900

    resultMesages.jsonを読みこんで表示する処理を追加

--- result.js
+++ result.js
@@ -8,35 +8,19 @@ import {
   ONE_WEEK,
 } from "./constants.js";
 
+import { readFile } from "fs/promises";
 export default class Result {
-  yourResult(totalMets) {
-    const { metsShortage, suggestedWalking, suggestedRunning } =
-      this.suggestedExercise(totalMets);
-    if (totalMets >= WEEKLY_DEAL_METS) {
-      console.log(
-        "素晴らしいですね！あなたの身体活動量は理想を上回っています！"
-      );
-    } else if (
-      totalMets === SUFFICIENT_METS ||
-      totalMets === SUFFICIENT_METS + 1
-    ) {
-      console.log("あなたの身体活動量は健康維持に適しています");
-    } else if (totalMets <= INSUFFICIENT_METS) {
-      console.log(`あなたの身体活動量は${metsShortage}Mets不足しています。`);
-      console.log("以下の運動量を1週間の内に追加すると理想値を上回ります");
-      console.log(
-        `ウォーキング: ${suggestedWalking}分以上→1日あたり${Math.trunc(suggestedWalking / 7)}分`
-      );
-      console.log(
-        `軽いランニング: ${suggestedRunning}分以上→1日あたり${Math.trunc(suggestedRunning / 7)}分`
-      );
-    }
+  async #parseJsonFile() {
+    const messageFile = await readFile("resultMessages.json", "utf-8");
+    const message = messageFile
+      .replaceAll("{totalMets}", this.totalMets)
+      .replaceAll("{metsShortage}", this.metsShortage);
+    return JSON.parse(message);
   }
 
-  suggestedExercise(totalMets) {
-    const metsShortage = WEEKLY_DEAL_METS - totalMets;
-    const suggestedWalking = Math.trunc((metsShortage / 0.4) * 6);
-    const suggestedRunning = Math.trunc((metsShortage / 0.9) * 6);
-    return { metsShortage, suggestedWalking, suggestedRunning };
+  #displayMessages(messages) {
+    messages.forEach((message) => console.log(message));
+  }
+
   }
 }
